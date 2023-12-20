e961c38b6da3fe2a67f764326f565905b57843ba
JT Cho <jtcho123@gmail.com>
2023-12-19 23:48:16 -0500

    create reducer

--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useReducer } from "react";
+import { createContext, useReducer } from "react";
 
 import { DUMMY_PRODUCTS } from "../dummy-products";
 
@@ -10,80 +10,88 @@ export const CartContext = createContext({
 });
 
 function shoppingCartReducer(state, action) {
-  if (action.type === 'ADD_ITEM') {
-    
+  if (action.type === "ADD_ITEM") {
+    const updatedItems = [...state.items];
+
+    const existingCartItemIndex = updatedItems.findIndex(
+      (cartItem) => cartItem.id === action.payload
+    );
+    const existingCartItem = updatedItems[existingCartItemIndex];
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      const product = DUMMY_PRODUCTS.find(
+        (product) => product.id === action.payload
+      );
+      updatedItems.push({
+        id: action.payload,
+        name: product.title,
+        price: product.price,
+        quantity: 1,
+      });
+    }
+
+    return {
+      ...state, // unnecessary as the state has only one value
+      items: updatedItems,
+    };
+  }
+
+  if (action.type === "UPDATE_ITEM") {
+    const updatedItems = [...state.items];
+    const updatedItemIndex = updatedItems.findIndex(
+      (item) => item.id === action.payload.productId
+    );
+
+    const updatedItem = {
+      ...updatedItems[updatedItemIndex],
+    };
+
+    updatedItem.quantity += action.payload.amount;
+
+    if (updatedItem.quantity <= 0) {
+      updatedItems.splice(updatedItemIndex, 1);
+    } else {
+      updatedItems[updatedItemIndex] = updatedItem;
+    }
+
+    return {
+      ...state,
+      items: updatedItems,
+    };
   }
 
   return state;
 }
 
 export default function CartContextProvider({ children }) {
-  const [shoppingCartState, shoppingCartDispatch] = useReducer(shoppingCartReducer, {
-    items:[],
-  });
+  // the second parameter of the useReducer sets an initial state
+  const [shoppingCartState, shoppingCartDispatch] = useReducer(
+    shoppingCartReducer,
+    {
+      items: [],
+    }
+  );
 
-  const [shoppingCart, setShoppingCart] = useState({
-    items: [],
-  });
+  // const [shoppingCart, setShoppingCart] = useState({
+  //   items: [],
+  // });
 
-  function handleAddItemToCart(id) {
-    shoppingCartDispatch({
-      type: 'ADD_ITEM',
-      payload: id,
-    });
-
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-
-      const existingCartItemIndex = updatedItems.findIndex(
-        (cartItem) => cartItem.id === id
-      );
-      const existingCartItem = updatedItems[existingCartItemIndex];
-
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          quantity: existingCartItem.quantity + 1,
-        };
-        updatedItems[existingCartItemIndex] = updatedItem;
-      } else {
-        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-        updatedItems.push({
-          id: id,
-          name: product.title,
-          price: product.price,
-          quantity: 1,
-        });
-      }
-
-      return {
-        items: updatedItems,
-      };
-    });
-  }
+  // function handleAddItemToCart(id) { removed as adding a reducer => 1
 
   function handleUpdateCartItemQuantity(productId, amount) {
-    setShoppingCart((prevShoppingCart) => {
-      const updatedItems = [...prevShoppingCart.items];
-      const updatedItemIndex = updatedItems.findIndex(
-        (item) => item.id === productId
-      );
-
-      const updatedItem = {
-        ...updatedItems[updatedItemIndex],
-      };
-
-      updatedItem.quantity += amount;
-
-      if (updatedItem.quantity <= 0) {
-        updatedItems.splice(updatedItemIndex, 1);
-      } else {
-        updatedItems[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        items: updatedItems,
-      };
+    shoppingCartDispatch({
+      type: "UPDATE_ITEM",
+      payload: {
+        // we can write codes this way when the var to name is the same as the parameter.
+        productId,
+        amount,
+      },
     });
   }
 
@@ -97,3 +105,62 @@ export default function CartContextProvider({ children }) {
     <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>
   );
 }
+
+// function handleAddItemToCart(id) {
+//   shoppingCartDispatch({
+//     type: 'ADD_ITEM',
+//     payload: id,
+//   });
+
+//   setShoppingCart((prevShoppingCart) => {
+//     const updatedItems = [...prevShoppingCart.items];
+
+//     const existingCartItemIndex = updatedItems.findIndex(
+//       (cartItem) => cartItem.id === id
+//     );
+//     const existingCartItem = updatedItems[existingCartItemIndex];
+
+//     if (existingCartItem) {
+//       const updatedItem = {
+//         ...existingCartItem,
+//         quantity: existingCartItem.quantity + 1,
+//       };
+//       updatedItems[existingCartItemIndex] = updatedItem;
+//     } else {
+//       const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+//       updatedItems.push({
+//         id: id,
+//         name: product.title,
+//         price: product.price,
+//         quantity: 1,
+//       });
+//     }
+
+//     return {
+//       items: updatedItems,
+//     };
+//   });
+// } => 1
+
+// setShoppingCart((prevShoppingCart) => {
+//   const updatedItems = [...prevShoppingCart.items];
+//   const updatedItemIndex = updatedItems.findIndex(
+//     (item) => item.id === productId
+//   );
+
+//   const updatedItem = {
+//     ...updatedItems[updatedItemIndex],
+//   };
+
+//   updatedItem.quantity += amount;
+
+//   if (updatedItem.quantity <= 0) {
+//     updatedItems.splice(updatedItemIndex, 1);
+//   } else {
+//     updatedItems[updatedItemIndex] = updatedItem;
+//   }
+
+//   return {
+//     items: updatedItems,
+//   };
+// });
